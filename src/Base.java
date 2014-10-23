62ca1d7067c841248883309898234f81082179f2
Jesse Chen <jchen428@gatech.edu>
2014-10-23 13:57:11 -0400

    Work assignments

--- src/Base.java
+++ src/Base.java
@@ -5,10 +5,10 @@ public class Base {
 	//Hannah
     //TO DO:
     //import matrix/vector basic functions(java.math??)
-    //1. inverse of a matrix
-    //2. QR factorization
-    //3. determinant
-    //4. trace
-    //5. eigenvalues and eigenvectors
-    //6. rotate, reflect, project vectors
+    //1. inverse of a matrix - Hannah
+    //2. QR factorization - Hannah
+    //3. determinant - Forrest
+    //4. trace - Forrest
+    //5. eigenvalues and eigenvectors - Jesse
+    //6. rotate, reflect, project vectors - Jesse
 }

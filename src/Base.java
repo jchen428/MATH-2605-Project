d8b2296dd09f25cf9ec3eac9432875cd35e4bc34
Jesse Chen <jchen428@gatech.edu>
2014-10-27 23:46:02 -0400

    Imported ojAlgo and made a test matrix.

--- src/Base.java
+++ src/Base.java
@@ -1,8 +1,9 @@
+import org.ojalgo.matrix.BasicMatrix;
+import org.ojalgo.matrix.BasicMatrix.Factory;
+import org.ojalgo.matrix.PrimitiveMatrix;
+
 
 public class Base {
-    //ForrestTest
-	//Jesse
-	//Hannah
     //TO DO:
     //import matrix/vector basic functions(java.math??)
     //1. inverse of a matrix - Hannah
@@ -11,4 +12,11 @@ public class Base {
     //4. trace - Forrest
     //5. eigenvalues and eigenvectors - Jesse
     //6. rotate, reflect, project vectors - Jesse
+	
+	public static void main(String[] args) {
+		Factory<PrimitiveMatrix> testFactory = PrimitiveMatrix.FACTORY;
+		double[][] testData = new double[][] {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
+		BasicMatrix<?> testMatrix = testFactory.rows(testData);
+		System.out.println(testMatrix.toString());
+	}
 }

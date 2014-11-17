e87bccf67f237b4957fd68f582e0614e9f731a3d
hecarlton <hcarlton3@gatech.edu>
2014-11-16 20:41:16 -0500

    d

---
+++ src/powermethod/PowerMethodMain.java
@@ -0,0 +1,14 @@
+package powermethod;
+import java.util.ArrayList;
+import org.jfree.chart.*;
+
+public class PowerMethodMain {
+	
+	public static void main(String[] args) {
+		
+		ArrayList list = PowerMethodConvergance.makeManyMatricies();
+		
+		
+		
+	}
+}

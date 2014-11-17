ab587763b4cdef164a068c7432294bde981c87e8
sfbengtson <bengtson94@gmail.com>
2014-11-16 19:56:45 -0500

    finished powermethod iterations and manymanymatrixs

--- src/powermethod/PowerMethodConvergance.java
+++ src/powermethod/PowerMethodConvergance.java
@@ -1,9 +1,15 @@
 package powermethod;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import basicfunctions.BasicFunctions;
+
 public class PowerMethodConvergance {
     
-    public float powerMethod (float[][] matrix, float[] vector, float tolerance, int MaxTimes) {
-	float error = 999999; //make this infinity
+    public static ArrayList powerMethod (float[][] matrix, float[] vector, float tolerance, int MaxTimes) {
+	ArrayList returnMe = new ArrayList();
+	float error = Float.POSITIVE_INFINITY; //make this infinity
 	float[] nextIterationVector = vector;
 	float eigenvalue = 0;
 	float[] eigenvector = new float[matrix.length];
@@ -25,17 +31,54 @@ public class PowerMethodConvergance {
 	    }
 	    eigenvector = resultVector;
 	    error = Math.abs(eigenvalue - previousEigenvalue);
-	    
 	    counter++;
 	}
-	//NOT SURE WHAT TO RETURN
-	return (float) 0.0;
+	if (error > tolerance) {
+	    return null; //return "null"
+	}
+	returnMe.add(eigenvector);
+	returnMe.add(eigenvalue);
+	returnMe.add(counter);
+	return returnMe; //return eigenvalue and eigenvector
     }
     
-    public void MakeManyMatricies() {
+    public static ArrayList makeManyMatricies() {
+	Random r = new Random();
 	int counter = 0;
-	while (counter < 1000) {
-	    
+	float [][] matrix = new float[2][2];
+	for (int i = 0; i < 1; i++) {
+	    for (int j = 0; j < 1; j++) {
+		float next = (r.nextFloat() * 4) - 2;
+		matrix[i][j] = next;
+	    }	
 	}
+	float det = BasicFunctions.determinant(matrix);
+	if (det == 0) {
+	    return null;
+	}
+	float[][] inverseMatrix = BasicFunctions.inverseOf2x2(matrix);
+	
+	float trace = BasicFunctions.trace(matrix);
+	float[] startingVector = new float[2];
+	for (int i = 0; i < startingVector.length; i++) {
+	    startingVector[i] = 1;
+	}
+	ArrayList data = powerMethod(matrix, startingVector, (float) 0.00005, 100);
+	if (data == null) {
+	    return null;
+	}
+	int numberOfIterations = (int) data.get(2);
+	ArrayList data2 = powerMethod(inverseMatrix, startingVector, (float) 0.00005, 100);
+	if (data == null) {
+	    return null;
+	}
+	int inverseNumberOfIterations = (int) data2.get(2);
+	ArrayList returnMe = new ArrayList();
+	returnMe.add(trace);
+	returnMe.add(det);
+	returnMe.add(numberOfIterations);
+	returnMe.add(inverseNumberOfIterations);
+	return returnMe;
     }
 }
+

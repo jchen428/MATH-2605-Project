52c59feec66dab51912cc968242e4dee434c4e27
hecarlton <hcarlton3@gatech.edu>
2014-11-20 14:58:14 -0500

    working on graphs

--- src/powermethod/PowerMethodMain.java
+++ src/powermethod/PowerMethodMain.java
@@ -1,14 +1,27 @@
 package powermethod;
 import java.util.ArrayList;
 
+import javafx.geometry.Rectangle2D;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel; 
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYDotRenderer;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.util.ShapeUtilities;
+
+import java.awt.Shape;
+
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.geom.Ellipse2D;
 
 public class PowerMethodMain {
 	
@@ -31,20 +44,52 @@ public class PowerMethodMain {
 			}
 		}
 		
-		XYDataset dataset = createDataset(determinants, traces);
-		JFreeChart chart = ChartFactory.createScatterPlot("Scatter Plot","Year","Paddy Production", dataset);
-		ChartPanel cp = new ChartPanel(chart);
-		cp.show();
+		//Plot number 1
+				// X axis is determinants, Y axis is traces
+				XYDataset dataset1 = createDataset(determinants, traces);
+				JFreeChart chart = ChartFactory.createScatterPlot("Determinants vs Traces - Coloring: Iterations","Determinants","traces", dataset1);
+				XYPlot xyPlot = (XYPlot) chart.getPlot();
+		        XYItemRenderer renderer = xyPlot.getRenderer();
+		        renderer.setSeriesPaint(0,(Paint) Color.BLACK);
+		        double size = 2;
+		        double delta = size / 2.0;
+		        Shape shape2 = new Ellipse2D.Double(-delta, -delta, size, size);
+		        renderer.setSeriesShape(0, shape2);
+				ChartPanel cp = new ChartPanel(chart);
+				JFrame frame = new JFrame();
+				frame.add(cp);
+			    frame.pack();
+			    frame.setVisible(true);
+	    
+	  //Plot number 2
+		// X axis is determinants, Y axis is traces
+			    
+			  //Plot number 1
+				// X axis is determinants, Y axis is traces
+				XYDataset dataset2 = createDataset(determinants, traces);
+				JFreeChart chart2 = ChartFactory.createScatterPlot("Determinants vs Traces - Coloring: Inverse Iterations","Determinants","traces", dataset2);
+		        XYPlot xyPlot2 = (XYPlot) chart2.getPlot();
+		        System.out.println(xyPlot2.getDatasetCount());
+		        XYItemRenderer renderer2 = xyPlot2.getRenderer();
+		        renderer2.setSeriesPaint(0,(Paint) Color.BLACK);
+		        double size2 = 2;
+		        double delta2 = size / 2.0;
+		        Shape shape3 = new Ellipse2D.Double(-delta2, -delta2, size2, size2);
+		        renderer2.setSeriesShape(0, shape3);
+				ChartPanel cp2 = new ChartPanel(chart2);
+				JFrame frame2 = new JFrame();
+				frame2.add(cp2);
+			    frame2.pack();
+			    frame2.setVisible(true);
 		
 	}
 	
-	//Plot number 1
-			// X axis is determinants, Y axis is traces
+	
 	
 	public static XYDataset createDataset(ArrayList det, ArrayList traces) {
 	        XYSeriesCollection result = new XYSeriesCollection();
 	        XYSeries series = new XYSeries("Plot 1");
-	        for (int t = 0; t <= 100; t++) {
+	        for (int t = 0; t < 1000; t++) {
 	            series.add((float) det.get(t), (float) traces.get(t));
 	        }
 	        result.addSeries(series);
